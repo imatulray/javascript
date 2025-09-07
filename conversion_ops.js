c98a53e0697da81466c2fab2fa19273b99b64b04
Atul <136067689+imatulray@users.noreply.github.com>
2025-09-07 14:23:41 +0000

    datatypes conversions

---
+++ conversion_ops.js
@@ -0,0 +1,49 @@
+/*Number Conversions*/
+
+//let score = ""
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let num_score = Number(score);
+// console.log(num_score);
+// console.log(typeof num_score);
+ 
+/* Type is getting changed in case of undefined and "33abc", but the value is not a number.  */
+// "33" => 33
+// "" => 0
+// null => 0
+// undefined => NaN
+// "33abc" => NaN
+
+/* Boolean Conversions*/
+
+// let isLoggedIn = "hitesh";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// null => false
+//undefined => false
+//"" => false
+//"atul" => true
+
+/* String Conversions */
+
+let marks = 100
+
+let stringMarks = String(marks)
+console.log(stringMarks);
+console.log(typeof stringMarks);
+
+// 100 => 100
+// null => null
+//undefined => undefined
+
+
+
+
+
+
+
